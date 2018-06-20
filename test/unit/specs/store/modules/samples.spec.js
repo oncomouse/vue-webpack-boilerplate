28b4fb003571a74d9751dd2257fc3322677ab6c1
oncomouse <oncomouse@gmail.com>
2018-06-20 13:04:34 -0500

    Clarified module tests

--- test/unit/specs/store/modules/samples.spec.js
+++ test/unit/specs/store/modules/samples.spec.js
@@ -5,18 +5,25 @@ import module, { ADD_SAMPLE_DONE, ERROR, RESET } from 'APP/store/modules/samples
 import post from 'TEST/fixtures/post.json';
 import { API_URL } from 'APP/api/config';
 
-const store = new Vuex.Store({
-  modules: {
-    samples: module,
-  },
-});
-
 describe('store/modules/samples', () => {
+  // We use a spy for a commit method when testing actions, instead of the full
+  // store, because we don't want to trigger our mutations during action tests.
   const commit = sinon.spy();
+  // Store is used for testing mutations:
+  const store = new Vuex.Store({
+    modules: {
+      samples: module,
+    },
+  });
+  const initialState = { ...module.state };
   afterEach(() => {
+    // Reset fetchMock:
     fetchMock.reset();
     fetchMock.restore();
+    // Reset our spy:
     commit.resetHistory();
+    // Reset our vuex store:
+    store.state.samples = { ...initialState };
   });
   // Boilerplate and setup tests:
   it('should initialize a blank array in state.samples', () => {
@@ -36,7 +43,8 @@ describe('store/modules/samples', () => {
       });
   });
   it('should commit ERROR when encountering a 404 error in actions.addSample', () => {
-    fetchMock.get(new RegExp(`${API_URL}/posts/[0-9]+.*$`), 404, { overwriteRoutes: true });
+    // Make fetchMock return a 404 error:
+    fetchMock.get(new RegExp(`${API_URL}/posts/[0-9]+.*$`), 404);
     return module.actions.addSample({ commit })
       .then(() => {
         expect(commit).to.be.calledOnce;
@@ -50,26 +58,20 @@ describe('store/modules/samples', () => {
   });
   // Mutation tests:
   it('should add one element to state.samples.samples when mutations[ADD_SAMPLE_DONE] is called.', () => {
-    const state = { ...module.state };
-    module.mutations[ADD_SAMPLE_DONE](state, post.title);
-    expect(state.samples).to.have.lengthOf(1);
+    module.mutations[ADD_SAMPLE_DONE](store.state.samples, post.title);
+    expect(store.state.samples.samples).to.have.lengthOf(1);
   });
   it('should reset element to state.samples.samples when mutations[RESET] is called.', () => {
-    const state = {
-      samples: [
-        post.title,
-        post.title,
-        post.title,
-      ],
-    };
-    module.mutations[RESET](state);
-    expect(state.samples).to.have.lengthOf(0);
+    store.state.samples.samples = [
+      post.title,
+      post.title,
+      post.title,
+    ];
+    module.mutations[RESET](store.state.samples);
+    expect(store.state.samples.samples).to.have.lengthOf(0);
   });
   it('should add one element to state.samples.errors when mutations[ERROR] is called.', () => {
-    const state = {
-      errors: [],
-    };
-    module.mutations[ERROR](state, { message: 'Test Message' });
-    expect(state.errors).to.have.lengthOf(1);
+    module.mutations[ERROR](store.state.samples, { message: 'Test Message' });
+    expect(store.state.samples.errors).to.have.lengthOf(1);
   });
 });
