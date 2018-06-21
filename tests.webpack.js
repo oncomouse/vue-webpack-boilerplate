d6920c3686150c86dd30d67eadd3fd3d18f4e95b
oncomouse <oncomouse@gmail.com>
2018-06-21 14:20:45 -0500

    Apparently this works best with NYC

--- tests.webpack.js
+++ tests.webpack.js
@@ -8,9 +8,12 @@ document.body.insertAdjacentHTML('beforeend', '<div id="root"></div>');
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
 
-const context = require.context('./test', true, /\.spec\.js$/);
-context.keys().forEach(context);
-
 global.expect = chai.expect;
 global.should = chai.should;
 global.assert = chai.assert;
+
+const context = require.context('./test', true, /\.spec\.js$/);
+context.keys().forEach(context);
+
+const srcContext = require.context('./app', true, /\.(?:vue|js)$/);
+srcContext.keys().forEach(srcContext);
